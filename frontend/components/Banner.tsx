a76a7b39baef9a045df1725940b3da216c5c7f1f
kazuki <kitano-kazuki954@e.c..u-tokyo.ac.jp>
2023-10-28 20:17:44 +0900

    banner base

---
+++ frontend/components/Banner.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Banner.css';
+
+function Banner() {
+  return (
+    <div className="banner">
+      {/* バナーのコンテンツをここに配置 */}
+    </div>
+  );
+}
+
+export default Banner;
