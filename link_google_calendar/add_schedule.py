1b87caf6b041bde8b6ca7a1d75c0701db0f53160
kitamurayuuka <82785221+0017-alt@users.noreply.github.com>
2023-10-28 14:03:31 +0900

    linking with google calendar

---
+++ link_google_calendar/add_schedule.py
@@ -0,0 +1,34 @@
+# before executing, just run `pip3 install google-api-python-client google-auth`!!
+
+import datetime
+from google.oauth2 import service_account
+from googleapiclient.errors import HttpError
+from googleapiclient.discovery import build
+
+# Create a new schedule
+    # |summary|    : the name of the event(string)
+    # |start_time| : the starting time (formated type such as: datetime.datetime(2023, 10, 28, 9, 0, 0))
+    # |end_time|   : the ending time (formated type such as: datetime.datetime(2023, 10, 28, 9, 0, 0))
+    # |calendar_id|: user email address
+
+def create_event(summary, start_time, end_time, calendar_id):
+    creds = service_account.Credentials.from_service_account_file('/content/drive/MyDrive/JPHACKS/jphack-tk-2306-100a02fa5204.json')
+    service = build('calendar', 'v3', credentials=creds)
+    event = {
+        'summary': summary,
+        'start': {
+            'dateTime': start_time.strftime('%Y-%m-%dT%H:%M:%S'),
+            'timeZone': 'Asia/Tokyo',
+        },
+        'end': {
+            'dateTime': end_time.strftime('%Y-%m-%dT%H:%M:%S'),
+            'timeZone': 'Asia/Tokyo',
+        },
+    }
+    try:
+        event = service.events().insert(calendarId=calendar_id, body=event).execute()
+        print(f'Event created: {event.get("htmlLink")}')
+    except HttpError as error:
+        print(f'An error occurred: {error}')
+        event = None
+    return event
