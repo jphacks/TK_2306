7a5c90a2bed190d490d7f8f2abc7d9e88e4150a0
kazuki <kitano-kazuki954@e.c..u-tokyo.ac.jp>
2023-10-29 14:57:25 +0900

    shift table look nice

--- frontend/pages/[id]/shift.tsx
+++ frontend/pages/[id]/shift.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
+import { Container, Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography, Box } from '@mui/material';
 import Banner from "../../components/Banner";
+import Theme from '../../components/Theme';
 
 type ConfirmedShift = {
   date: string;
@@ -26,38 +28,67 @@ const ShiftConfirmationPage: React.FC = () => {
     setUserName('John Doe');
   }, []);
 
-  const handleAddToGoogleCalendar = async () => {
+  const handleAddToGoogleCalendar = () => {
+    // Logic to add the confirmed shifts to Google Calendar
     console.log('Adding to Google Calendar:', confirmedShifts);
   };
 
   return (
-    <div>
-      <Banner />
-      <div>
-        <h1>シフト確認画面</h1>
-        <h2>{userName}</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>From</th>
-              <th>To</th>
-            </tr>
-          </thead>
-          <tbody>
-            {confirmedShifts.map((shift, index) => (
-              <tr key={index}>
-                <td>{shift.date}</td>
-                <td>{shift.from}</td>
-                <td>{shift.to}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <button onClick={handleAddToGoogleCalendar}>Google Calendarに登録</button>
-      </div>
-    </div>
+    <Container disableGutters maxWidth={false}>
+      <Theme>
+        <Banner />
+        <Container maxWidth={false} sx={{ paddingTop: 2, paddingBottom: 5, paddingLeft: 5, paddingRight: 5 }}>
+          <Typography variant="h4">シフト確認画面</Typography>
+
+          <Box display="flex" justifyContent="center">
+            <TableContainer component={Paper} style={{ width: '80%', maxWidth: '1000px' }}>
+              <Table stickyHeader >
+                <TableBody>
+                  <TableRow  >
+                    <TableCell sx={{ backgroundColor: 'rgba(217, 217, 217, 0.38)',color: 'black' }}>{" "}</TableCell>
+                    <TableCell sx={{ backgroundColor: 'lightgrey', borderLeft: '1px solid black' }}>{"2023/11/01"}</TableCell>
+                    <TableCell sx={{ backgroundColor: 'lightgrey',orderLeft: '1px solid black' }}>{"2023/11/02"}</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell sx={{ backgroundColor: 'rgba(217, 217, 217, 0.38)',orderLeft: '1px solid black' }}>{" "}</TableCell>
+                    <TableCell sx={{ backgroundColor: 'lightgrey',borderLeft: '1px solid black' }}>{"9:00"}~{"17:00"}</TableCell>
+                    <TableCell sx={{ backgroundColor: 'lightgrey',borderLeft: '1px solid black' }}>{"10:00"}~{"18:00"}</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell sx={{ backgroundColor: 'lightgrey',orderLeft: '1px solid black' }}>{userName}</TableCell>
+                    {confirmedShifts.map((shift, index) => (
+                      <React.Fragment key={index}>
+                        <TableCell sx={{ borderLeft: '1px solid black' }}>{shift.from}~{shift.to}</TableCell>
+                      </React.Fragment>
+                    ))}
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Box>
+
+          <Box mt={2} display="flex" justifyContent="center">
+            <Button variant="contained" color="primary" onClick={handleAddToGoogleCalendar} style={{ marginTop: '10px', width: '200px', height: '50px' }}>
+              New Register
+            </Button>
+          </Box>
+          <Box mt={20} display="flex" justifyContent="center">
+            <Button variant="contained" color="primary" onClick={handleAddToGoogleCalendar} style={{ marginTop: '10px', width: '400px', height: '70px' }}>
+              Go
+            </Button>
+          </Box>
+        </Container>
+      </Theme>
+    </Container>
+
+
   );
 };
 
 export default ShiftConfirmationPage;
+
+
+
+
+
+
